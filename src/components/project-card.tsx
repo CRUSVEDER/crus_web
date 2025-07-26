306bdf5d526a546046720f8f82df771351d471ce
CRUSVEDER <yashgholap777@gmail.com>
2025-07-27 03:18:37 +0530

    changed project

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -29,8 +29,9 @@ const ProjectCard = ({ project, index }: Props) => {
       }}
       className={`opacity-0 transition-transform duration-300 hover:rotate-[var(--imagerotation)] hover:-translate-y-4 animate-fade_in ease-[var(--ease-elastic-out-2)]`}
     >
-      <div className="relative bg-gray-50 rounded-xl shadow-sm dark:bg-gray-700 group">
-        <div className="flex relative z-10 flex-col bg-gray-50 rounded-xl transition duration-500 dark:bg-gray-800">
+      <div className="relative bg-gray-50 rounded-md shadow-md border border-gray-200 dark:border-gray-600 dark:bg-gray-700 group">
+
+        <div className="flex relative z-10 flex-col bg-gray-50 rounded-md transition duration-500 dark:bg-gray-800">
           {url && (
             <Link
               href={url}
@@ -38,7 +39,7 @@ const ProjectCard = ({ project, index }: Props) => {
               tabIndex={-1}
               target="_blank"
               rel="noopener noreferrer"
-              className="overflow-y-hidden z-10 mb-2 w-full h-32 bg-gray-200 rounded-t-xl dark:bg-gray-600 hover:cursor-pointer"
+              className="overflow-y-hidden z-10 mb-2 w-full h-32 bg-gray-200 rounded-t-md dark:bg-gray-600 hover:cursor-pointer"
             >
               <Parallax translateY={[0, randomNumber(60, 120)]} className="relative h-full">
                 <Image
@@ -49,7 +50,7 @@ const ProjectCard = ({ project, index }: Props) => {
                   sizes={"100%"}
                   width="320"
                   height="120"
-                  className="object-cover object-top w-full rounded-t-xl"
+                  className="object-cover object-top w-full rounded-t-md"
                 />
               </Parallax>
             </Link>
@@ -61,7 +62,7 @@ const ProjectCard = ({ project, index }: Props) => {
                 href={url}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="items-center p-1 font-mono font-thin text-gray-600 rounded-sm transition-all duration-300 outline-none md:text-lg dark:text-gray-100 hover:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-offset-2 focus:ring-offset-gray-100 focus:outline-none flex-inline dark:hover:text-palevioletred hover:text-palevioletred focus:dark:ring-palevioletred focus:dark:ring-offset-gray-800"
+                className="items-center p-1 font-mono font-thin text-gray-600 rounded-md transition-all duration-300 outline-none md:text-lg dark:text-gray-100 hover:outline-none focus:ring-2 focus:ring-pink-300 focus:ring-offset-2 focus:ring-offset-gray-100 focus:outline-none flex-inline dark:hover:text-palevioletred hover:text-palevioletred focus:dark:ring-palevioletred focus:dark:ring-offset-gray-800"
               >
                 <span>{name}</span>
                 <svg
@@ -88,7 +89,7 @@ const ProjectCard = ({ project, index }: Props) => {
             </div>
           </div>
 
-          <div className="flex relative z-10 p-6 align-middle rounded-b-xl">
+          <div className="flex relative z-10 p-6 align-middle rounded-b-md">
             <div className="flex overflow-hidden relative z-10 justify-around w-full">
               {tech &&
                 tech.map((type) => (
